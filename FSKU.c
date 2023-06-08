272a5d2e9005867e895715977c94e1311206d69a
hassen <hassen@hassen.com>
2023-06-08 22:38:00 +0900

    done : init storage with super, ibmap,dbmap with offset

--- FSKU.c
+++ FSKU.c
@@ -6,11 +6,11 @@ int main(int argc, char* argv){
 
     unsigned int SIZE_STORAGE = 32*1024; //32KB mem
     unsigned int SIZE_BLOCK = 512;
-    unsigned int offset_SUPER = SIZE_BLOCK*0;
+    unsigned int offset_SUPER = 0;
     unsigned int offset_IBMAP = SIZE_BLOCK;
     unsigned int offset_DBMAP = SIZE_BLOCK+256;
-    unsigned int offset_INODE1 = SIZE_BLOCK*2;
-    unsigned int offset_INODE2 = SIZE_BLOCK*3;
+    unsigned int offset_INODE0 = SIZE_BLOCK*2;
+    unsigned int offset_INODE1 = SIZE_BLOCK*3;
     unsigned int offset_DBLOCK = SIZE_BLOCK*4;
 
     typedef struct _inode{
@@ -22,20 +22,27 @@ int main(int argc, char* argv){
     
     char* storage = (char*)malloc(SIZE_STORAGE); //char=1byte
 
-    memset(storage,'1',SIZE_STORAGE); // init whole storage blocks with nulchar
+    memset(storage,'\0',SIZE_STORAGE); // init whole storage blocks with nulchar
 
-
-    /* init super */
-    for(unsigned int i=0;i<offset_SUPER+SIZE_BLOCK;i++){
-        *(storage+i)='S';
-        printf("%c",*(storage+i));
-    }
+    printf("### FCKU ###\n");
 
     /* init super */
-    for(unsigned int i=0;i<offset_SUPER+SIZE_BLOCK;i++){
-        *(storage+i)='S';
-        printf("%c",*(storage+i));
+    for(unsigned int i=offset_SUPER;i<SIZE_BLOCK;i++){
+        *(storage+i)='0'; // fill super block with 0
     }
+    /* init i-bmap*/
+    for(unsigned int i=offset_IBMAP;i<offset_IBMAP+SIZE_BLOCK;i++){
+        *(storage+i)='b'; // fill super block with 0
+    }
+    *(storage+offset_IBMAP)='R'; // 삭제 비트 나중에 0으로 수정
+    *(storage+offset_IBMAP+1)='B'; // 배드 비트 나중에 0으로 수정
+
+    /* init i-bmap*/
+    for(unsigned int i=offset_DBMAP;i<offset_DBMAP+SIZE_BLOCK;i++){
+        *(storage+i)='r'; // fill super block with 0
+    }
+    *(storage+offset_DBMAP)='1'; // root dir bit
+    
 
     /*print whole memory*/
     for(unsigned int i=0;i<SIZE_STORAGE;i++){
