9ca98a264a92ceadadc8c298790fba6ec17e4ffb
hassen <hassen@hassen.com>
2023-06-08 14:53:19 +0900

    changed size_t to unsigned int

--- FSKU.c
+++ FSKU.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv){
 
-    size_t size = 32*1024; //32KB mem
+    unsigned int size = 32*1024; //32KB mem
     char* buffer = (char*)malloc(size); //char=1byte
 
+    memset(buffer,'1',size);
+
+    for(size_t i=0;i<size;i++){
+        printf("%c",buffer[i]);
+    }
+    printf("\n");
 
     free(buffer);
 
