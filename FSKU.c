2c552d0366982840fe4ac627d6410bfad9e29997
hassen <hassen@hassen.com>
2023-06-08 17:37:02 +0900

    fill super with nul char

--- FSKU.c
+++ FSKU.c
@@ -5,20 +5,25 @@
 int main(int argc, char* argv){
 
     unsigned int SIZE_STORAGE = 32*1024; //32KB mem
-    unsigned int SUPER = 512;
-    char* buffer = (char*)malloc(SIZE_STORAGE); //char=1byte
+    unsigned int SIZE_SUPER = 512;
+    char* storage = (char*)malloc(SIZE_STORAGE); //char=1byte
 
-    memset(buffer,'0',SIZE_STORAGE);
+    memset(storage,'\0',SIZE_STORAGE);
 
     //printf("size : %u\n",sizeof(SIZE_STORAGE));
 
-    //print memory
-    for(unsigned int i=0;i<SIZE_STORAGE;i++){
-        printf("%c",buffer[i]);
-    }
-    printf("\n");
+    /*print whole memory*/
+    // for(unsigned int i=0;i<SIZE_STORAGE;i++){
+    //     printf("%c",storage[i]);
+    // }
+    // printf("\n");
 
-    free(buffer);
+    /* print super */
+    for(unsigned int i=0;i<SIZE_SUPER;i++){
+        printf("%c",storage[i]);
+    }
+
+    free(storage);
 
 
     return 0;
