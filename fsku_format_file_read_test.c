5114530fe318eced28a7c391388f11384ec345c5
hassen <hassen@hassen.com>
2023-06-08 21:50:14 +0900

    fsku file reader without while loop done

---
+++ fsku_format_file_read_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc,char* argv[]){
+    
+    FILE* file;
+    file = fopen(argv[1],"r");
+
+    char line_buffer[100];
+
+    typedef struct _file_ent{
+        char filename[5];
+        char iocommand[5];
+        int filelen;
+    }file_ent;
+
+    file_ent line_data;
+    fscanf(file,"%s %s %d\n",line_data.filename,line_data.iocommand,&line_data.filelen);
+    printf("%s %s %d\n",line_data.filename,line_data.iocommand,line_data.filelen);
+
+
+    fclose(file);
+
+    return 0;
+}
